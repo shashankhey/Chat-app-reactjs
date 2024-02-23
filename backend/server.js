652dc1a104e0abe55affa9b2a3c693390fc8302c
shash <shashankhey1710@gmail.com>
2024-02-23 12:53:46 +0530

    Socket Addes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js';
 
-const app = express()
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server listening on port ${PORT}`)
 })
